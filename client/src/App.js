d2d4d38f592c4aefe75638e5d0c6abf23bd694f8
JaiRaga <raga.j.santhosh@gmail.com>
2020-06-04 15:33:25 +0530

    created react app, added color palette

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Palette from "./component/Palette";
+import seedPalette from "./seedPalette";
+
+function App() {
+  return (
+    <div className='App'>
+      <Palette {...seedPalette} />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/component/Palette.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Palette = () => {
+  return (
+    <div className='Palette'>
+      {/* Navbar goes here */}
+      <div className='Palette-colors'>{/*Bunch of Colors goes here*/}</div>
+      {/* Footer goes here */}
+    </div>
+  );
+};
+
+export default Palette;
