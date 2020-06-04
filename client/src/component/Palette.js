76ebbab76c77fbe5e719bdfa5b40ef615c8aa7e5
JaiRaga <raga.j.santhosh@gmail.com>
2020-06-04 20:33:52 +0530

    added colorBox component

--- client/src/component/Palette.js
+++ client/src/component/Palette.js
@@ -1,10 +1,15 @@
 import React from "react";
+import ColorBox from "./ColorBox";
+import "./Palette.css";
 
-const Palette = () => {
+const Palette = ({ colors }) => {
+  const colorBox = colors.map((color) => {
+    return <ColorBox background={color.color} name={color.name} />;
+  });
   return (
     <div className='Palette'>
       {/* Navbar goes here */}
-      <div className='Palette-colors'>{/*Bunch of Colors goes here*/}</div>
+      <div className='Palette-colors'>{colorBox}</div>
       {/* Footer goes here */}
     </div>
   );
